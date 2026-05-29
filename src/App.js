846af5a16f0aa045a9a0d41f7fe22e63ef7b8bf7
Mohanakumaran K <mohanakumarank78@gmail.com>
2026-05-29 23:25:33 +0530

    Connected React frontend with FastAPI backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+
+  const analyzeResume = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/analyze");
+
+      const data = await response.json();
+
+      console.log(data);
+
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>AI Resume Analyzer</h1>
+
+      <button onClick={analyzeResume}>
+        Analyze Resume
+      </button>
     </div>
   );
 }
